d23ecb07b0b833eb20bc66fe81d0b3068f9555f4
santiago_viale <santicbsn9@gmail.com>
2024-11-25 12:36:23 -0300

    primer commit

---
+++ OneDrive/Escritorio/Portfolio-Dos/portfolio-project/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../../assets/Santiago-Viale-Sistemas.png'
+import './NavBar.css'; // Asegúrate de tener un archivo CSS para estilos
+
+const NavBar: React.FC = () => {
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Desplazamiento suave
+    }
+  };
+  return (
+    <nav className="navbar">
+      <div >
+      <a onClick={() => scrollToSection('home')}><img className="logo" src={logo} alt="" /></a>
+      </div>
+      <ul className="nav-links">
+        <li>
+        <a onClick={() => scrollToSection('projects')}>Projects</a>
+        </li>
+        <li>
+          <a href='#aboutme'>About Me</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
